2cea53b6d4b28897fdc1780716132b179f0c0b69
OlegYap1 <legiggid@gmail.com>
2025-10-20 14:48:36 +0300

    feat: add Redis in ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,31 +6,50 @@ use App\Http\Requests\PaginationRequest;
 use App\Http\Requests\ProductRequest;
 use App\Http\Resources\ProductResource;
 use App\Models\Product;
+use Illuminate\Support\Facades\Cache;
 
 class ProductController extends Controller
 {
     public function index(PaginationRequest $request)
     {
-        $query = Product::query();
+        $perPage = $request->perPage();
 
-        return ProductResource::collection(
-            $query->paginate($request->perPage())
-        );
+        $page = $request->input('page', 1);
+
+        $cacheKey = "products_page_{$page}_per_{$perPage}";
+
+        $products = Cache::remember($cacheKey,now()->addDay(), function () use ($perPage) {
+            return Product::paginate($perPage);
+        });
+
+        return ProductResource::collection($products);
     }
 
     public function store(ProductRequest $request): ProductResource
     {
-        return new ProductResource(Product::create($request->validated()));
+        $product = Product::create($request->validated());
+
+        Cache::flush();
+
+        return new ProductResource($product);
     }
 
     public function show(Product $product): ProductResource
     {
-        return new ProductResource($product);
+        $cacheKey = "products_show_{$product->id}";
+
+        $cachedProduct = Cache::remember($cacheKey,now()->addDay(), function () use ($product) {
+            return $product;
+        });
+
+        return new ProductResource($cachedProduct);
     }
 
     public function update(ProductRequest $request, Product $product): ProductResource
     {
         $product->update($request->validated());
+
+        Cache::forget("product_{$product->id}");
 
         return new ProductResource($product);
     }
@@ -38,6 +57,9 @@ class ProductController extends Controller
     public function destroy(Product $product)
     {
         $product->delete();
+
+        Cache::forget("products_{$product->id}");
+        Cache::flush();
 
         return response()->noContent();
     }
